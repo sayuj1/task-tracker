5df3d6650226631585a66dce8eed48a8484cadc1
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-11-05 16:36:07 +0530

    card textarea padding fixed and font size fixed

--- views/MainPage.php
+++ views/MainPage.php
@@ -30,17 +30,19 @@ $conn = openConn();
 </head>
 
 <body>
+    <!-- top bar -->
     <div class='top-bar'>
         <div class="valign-wrapper">
             <div class="left-align" style="width:50%;">Task Tracker</div>
             <div class="right-align" style="width:50%;">Howdy, <?php echo $_SESSION['username'] ?></div>
         </div>
     </div>
+    <!-- for loading the -->
     <img src="../assets/imgs/table-task-loader.gif" id="preloader" alt="your browser does not support gifs!"
         width="100%" height="50%" style="margin: 0 auto">
     <div class="main-container" style="display: none;">
 
-        <!-- For confirming the card submission -->
+        <!-- Showing the confirmation of card creation -->
         <div id="modal3" class="modal">
             <div class="modal-content center-align">
                 <h4>Card Created Successfully!</h4>
@@ -81,9 +83,9 @@ $conn = openConn();
                                                 Task-Details:
                                             </label>
                                         </div>
-                                        <div class="col s6 m6 push-m2">
+                                        <div class="col s6 m6 push-m2 flow-text">
                                             <textarea name="tasks" id="card-body" placeholder="Your Tasks goes here...."
-                                                style="max-width:100%;height:200px;resize:none" required></textarea>
+                                                style="max-width:100%;height:200px;resize:none;padding: 14px" required></textarea>
                                         </div>
                                     </div>
                                     <div class="row left-align">
@@ -164,9 +166,9 @@ $conn = openConn();
                                                 Task-Details:
                                             </label>
                                         </div>
-                                        <div class="col s12">
+                                        <div class="col s12 flow-text">
                                             <textarea name="tasks" id="card-body" placeholder="Your Tasks goes here...."
-                                                style="max-width:100%;height:200px;resize:none" required></textarea>
+                                                style="max-width:100%;height:200px;resize:none;padding: 14px;" required></textarea>
                                         </div>
                                     </div>
                                     <div class="row left-align">
