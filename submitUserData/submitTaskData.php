ccecd01f2f21146544f0ce8978e58594dcb4adc1
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-11-11 18:06:39 +0530

    timezone bug fixed | date display format fixed

--- submitUserData/submitTaskData.php
+++ submitUserData/submitTaskData.php
@@ -8,11 +8,16 @@ $conn = openConn();
 $username = $_SESSION['username'];
 $cardTitle = mysqli_real_escape_string( $conn, $_POST['title'] );
 $cardTask = mysqli_real_escape_string( $conn, $_POST['tasks'] );
+
+date_default_timezone_set( 'Asia/Kolkata' );
 $dateStarted = date( 'Y-m-d H:i:s' );
+$dateObj = date_create( $dateStarted );
+$dateFinal = date_format( $dateObj, 'j F Y :: H:i:s' );
+
 $taskStatus = mysqli_real_escape_string( $conn, $_POST['status'] );
 $assignedBy = mysqli_real_escape_string( $conn, $_POST['assignedby'] );
 
-$query = "INSERT INTO `cardtask` (`id`,`username`,`title`,`task`,`datestarted`,`datecompleted`,`status`,`assignedby`) VALUES('','".$username."','".$cardTitle."','".$cardTask."','".$dateStarted."','','".$taskStatus."','".$assignedBy."')";
+$query = "INSERT INTO `cardtask` (`id`,`username`,`title`,`task`,`datestarted`,`datecompleted`,`status`,`assignedby`) VALUES('','".$username."','".$cardTitle."','".$cardTask."','".$dateFinal."','','".$taskStatus."','".$assignedBy."')";
 
 if ( mysqli_query( $conn, $query ) ) {
     echo 'insertionSuccessful';
