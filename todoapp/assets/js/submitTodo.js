3ffeb21c5186b6f8e9e725ded4093517f4ce69f7
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-11-23 00:57:07 +0530

    minor bug fixed

--- todoapp/assets/js/submitTodo.js
+++ todoapp/assets/js/submitTodo.js
@@ -10,56 +10,52 @@ function prependTheLatestTodo(container, data) {
         `<div class='col s12 m8' id=` +
         data[0]["todoId"] +
         `>
-        <div class='card-panel red accent-3 flow-text'>
-            <div class="row">
-                <div class="left-align col s6 white-text flow-text">
-                ` +
+    <div class='card-panel red accent-3 flow-text'>
+        <div class="row">
+            <div class="left-align col s6 white-text flow-text">
+            ` +
         data[0]["datecreated"].split("::")[0] +
         `
-                </div>
-                <div class='right-align col s6'>
-                    <!-- Dropdown Trigger -->
-                    <a class='todo-setting-trigger' data-target='todo-setting` + data[0]["todoId"] + `'>
-                        <i class='material-icons white-text'>settings</i>
-                    </a>
+            </div>
+            <div class='right-align col s6'>
+                <!-- Dropdown Trigger -->
+                <a class='todo-setting-trigger' data-target='todo-setting` + data[0]["todoId"] + `'>
+                    <i class='material-icons white-text'>settings</i>
+                </a>
 
-                    <!-- Dropdown Structure -->
-                    <ul id='todo-setting` + data[0]["todoId"] + `' class='dropdown-content'>
-                        <li><a style="color: #2196f3;" id=` +
+                <!-- Dropdown Structure -->
+                <ul id='todo-setting` + data[0]["todoId"] + `' class='dropdown-content'>
+                    <li><a style="color: #2196f3;" id=` +
         data[0]["todoId"] +
-        `><i class='material-icons left blue-text'>mode_edit</i>Edit</a>
-                        </li>
-                        <li><a style="color: red;" id=` +
+        ` onclick=todoEdit(this.id)><i class='material-icons left blue-text'>mode_edit</i>Edit</a>
+                    </li>
+                    <li><a style="color: red;" id=` +
         data[0]["todoId"] +
         ` onclick=todoDelete(this.id)><i class='material-icons left red-text'>delete</i>Delete</a>
-                        </li>
-                        <li><a style="color: green;" id=` +
+                    </li>
+                    <li><a style="color: green;" id=` +
         data[0]["todoId"] +
-        `><i class='material-icons left green-text'>check</i>Completed</a>
-                        </li>
-                        <li><a style="color: #ff9800;" id=` +
+        ` onclick=todoComplete(this.id)><i class='material-icons left green-text'>check</i>Completed</a>
+                    </li>
+                    <li><a style="color: #ff9800;" id=` +
         data[0]["todoId"] +
-        `><i class='material-icons left orange-text'>close</i>Pending</a>
-                        </li>
-                    </ul>
+        ` onclick=todoPending(this.id)><i class='material-icons left orange-text'>close</i>Pending</a>
+                    </li>
+                </ul>
 
-                </div>
             </div>
-
-            <div class='white-text' style='text-align: justify'>
-            ` +
-        data[0]["todo"] +
-        `
-            </div>
-            <br>
-                    <div class="divider"></div>
-                    <div class=" ` +
-        statusArr[data[0]["status"]] +
-        `right-align" style="margin-top: 10px;font-weight: 700;">
-                    Status: ` + data[0]["status"] + `
-                    </div>
         </div>
-    </div>`
+        <div class = "white-text card-content"
+        style = "text-align: justify;">
+        ` + data[0]["todo"] +
+        ` </div> 
+            <br>
+        <div class = "divider" > </div> <div class = "` + statusArr[data[0]["status"]] +
+        `right-align todoStatus" style="margin-top: 10px;font-weight: 700;">
+                Status: ` + data[0]["status"] + `
+                </div>
+    </div>
+</div>`
     );
 }
 
